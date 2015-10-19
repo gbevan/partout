f9cc22bddf3374a9d9b9c22e7386d713e53685df
Graham Bevan <graham.bevan@ntlworld.com>
2015-10-19 22:12:28 +0100

    tar strip top dir

--- bin/prep-nodejs.sh
+++ bin/prep-nodejs.sh
@@ -13,4 +13,4 @@ NODE_LINUX_X64=https://nodejs.org/dist/v4.2.1/node-v4.2.1-linux-x64.tar.gz
 # TODO: add other platforms
 
 mkdir -p node/linux/x64
-curl $NODE_LINUX_X64 | tar zxvf -C node/linux/x64
+curl $NODE_LINUX_X64 | tar zxv -C node/linux/x64 --strip-components=1
